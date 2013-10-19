88895e3e74e4dad30945a1c3113567a31fdb12a1
Dave J <contact_davej@ymail.com>
2013-10-19 17:10:51 +0200

    event.Raise<> added. No tests for it. (I know, it works :P)

---
+++ Csharp-Extensions/GeneralExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CsExtensions
+{
+    public static class GeneralExtensions
+    {
+        /// <summary>
+        /// Easy and save method to raise
+        /// an event. Encapsules the null-check.
+        /// </summary>
+        /// <typeparam name="T">EventArgs-type</typeparam>
+        /// <param name="handler">The event itself.</param>
+        /// <param name="sender">The sender.</param>
+        /// <param name="ev">The EventArgs.</param>
+        public static void Raise<T>(this EventHandler<T> handler, object sender, T ev) where T : EventArgs
+        {
+            if (handler != null)
+                handler.Invoke(sender, ev);
+        }
+    }
+}
